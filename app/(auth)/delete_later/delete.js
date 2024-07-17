1cfb92570321d00074620d855acecb4d9e20131e
Rigved Patil <rigved.patil@abbott.com>
2024-07-16 19:56:20 -0700

    Button1

---
+++ app/(auth)/delete_later/delete.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const delet1e = () => {
+  return (
+    <View>
+      <Text>delete</Text>
+    </View>
+  )
+}
+
+export default delete1
